d90b2338b11a4745bb0e326838776ea6a4268eb4
marieroxanne <ndatimanamarieroxanne@gmail.com>
2026-07-18 13:30:44 +0200

    j'ai fais la modification dans le controller et databse

--- App/core/Controller.php
+++ App/core/Controller.php
@@ -1,1 +1,11 @@
 # Classe mère des contrôleurs
+
+<?php
+    namespace app\core ;
+    class controller{
+        protected function renderview($view,$data){
+        }
+        protected function sendjson($data,$status){
+        }
+    }
+?>
